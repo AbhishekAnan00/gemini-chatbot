f8f431ea4d874785afb50f3b479c268de72c87ce
AbhishekAnan00 <email.abhishek00@gmail.com>
2025-03-22 03:16:51 +0530

    eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -9,6 +9,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+//const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+
+
+const rawConfig = compat.extends("next/core-web-vitals");
+const eslintConfig = [...new Set(rawConfig)];
 
 export default eslintConfig;
